f69f85dfc300cbabf52d5c6fb6fe7b1a73ce66cd
金鹏 <137772764@qq.com>
2018-01-04 11:51:52 +0800

    下拉刷新功能,今天不想写了

---
+++ src/common/pulldown/PullDown.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="pull-down-wrap" :style="`height: ${height}px;width: ${height * 2}px;top: -${height}px`">
+		<template v-if="height < 50">
+			<div class="pull-down-circle"></div>
+			<div class="pull-down-circle big"></div>
+			<div class="pull-down-circle"></div>
+		</template>
+		<span v-else>松开刷新</span>
+	</div>
+</template>
+
+<script>
+	import './pulldown.styl'
+	export default{
+		props: ['height', 'isRefresh']
+	}
+</script>
--- src/components/recommend/Recommend.vue
+++ src/components/recommend/Recommend.vue
@@ -1,19 +1,20 @@
 <template>
-  <Scroll :refresh="refreshScroll" @pulldown="handlePullDown">
-    <div class="music-recommend">
-      <div class="slider-container">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide" v-for="(slider, index) in slideList">
-            <a class="slider-nav">
-              <img :src="slider.picUrl" alt="推荐" width="100%" height="100%">
-            </a>
-          </div>
-        </div>
-        <div class="swiper-pagination"></div>
-      </div>
-      <div class="album-container">
-        <h1 class="title">最新专辑</h1>
-        <div class="album-list">
+	<Scroll :refresh="refreshScroll" @pulldown="handlePullDown" @scroll="handleScroll">
+		<div class="music-recommend">
+			<pull-down :height="pullDownHeight"></pull-down>
+			<div class="slider-container">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide" v-for="(slider, index) in slideList">
+						<a class="slider-nav">
+							<img :src="slider.picUrl" alt="推荐" width="100%" height="100%">
+						</a>
+					</div>
+				</div>
+				<div class="swiper-pagination"></div>
+			</div>
+			<div class="album-container">
+				<h1 class="title">最新专辑</h1>
+				<div class="album-list">
 					<div class="album-wrapper" @click="toAlbumDetail(album)"
 							 v-for="album in newAlbums.map(item => albumCreateByList(item))">
 						<div class="left">
@@ -25,12 +26,12 @@
 						<div class="public-time">{{album.publicTime}}</div>
 					</div>
 					</div>
-        </div>
-      </div>
-    </div>
-    <Loading title="正在加载..." :show="loading"></Loading>
-    <router-view/>
-  </Scroll>
+				</div>
+			</div>
+		</div>
+		<Loading title="正在加载..." :show="loading"></Loading>
+		<router-view/>
+	</Scroll>
 </template>
 <script>
   import {getCarousel, getNewAlbum} from '@/api/recommend'
@@ -38,6 +39,7 @@
   import Swiper from 'swiper'
   import Scroll from '@/common/scroll/Scroll'
   import Loading from '@/common/loading/Loading'
+  import PullDown from '@/common/pulldown/PullDown'
 	import * as AlbumModel from '@/model/album'
 
   import './recommend.styl';
@@ -45,7 +47,7 @@
 
   export default {
     components: {
-      Scroll, Loading
+      Scroll, Loading, PullDown
     },
     data() {
       return {
@@ -54,6 +56,7 @@
         newAlbums: [],
         refreshScroll: false,
         sliderSwiper: null,
+				pullDownHeight: 0,
       }
     },
     mounted() {
@@ -91,6 +94,9 @@
 				if(y > 50){
 					this.getNewAlbum();
 				}
+			},
+			handleScroll({y}){
+				this.pullDownHeight = y;
 			},
 			getNewAlbum(){
 				getNewAlbum().then((res) => {
